ff0b337d6caee41c5b70f53cb7e6b1667a104d40
山口泰生 <yamaguchi@s-MacBok-Pro.local>
2026-02-11 17:16:15 +0900

    imp: nvim, pluginの更新でのenter入力を省く

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,5 +25,7 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
+  -- リロードは維持、通知のみオフ
+  change_detection = { notify = false },
 })
